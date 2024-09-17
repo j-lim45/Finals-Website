b5d0e68080dfe19134af7ccdf20c0e27e22c5632
j-lim45 <you@example.com>
2024-09-17 19:46:30 +0800

    added the second tab

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 function printLorem() {
-    
+    document.getElementsByTagName("p").innerHTML = "SDA ipsum dolor sit amet, consectetur adipiscing elit. Cras ut venenatis ex. Nunc facilisis tempor metus, eu posuere libero viverra sit amet. Nam leo mauris, aliquet sed justo ut, dignissim placerat justo.";
 }
+
+printLorem();
