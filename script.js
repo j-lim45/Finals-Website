8eb7ff06372657197f6bc7cac77ff5fe51e4f436
j-lim45 <you@example.com>
2024-09-20 00:50:42 +0800

    ugh

--- script.js
+++ script.js
@@ -1,5 +1,52 @@
-function printLorem() {
-    document.getElementsByTagName("p").innerHTML = "SDA ipsum dolor sit amet, consectetur adipiscing elit. Cras ut venenatis ex. Nunc facilisis tempor metus, eu posuere libero viverra sit amet. Nam leo mauris, aliquet sed justo ut, dignissim placerat justo.";
-}
+(function(){
 
-printLorem();
+    var doc = document.documentElement;
+    var w   = window;
+
+    var curScroll;
+    var prevScroll = w.scrollY || doc.scrollTop;
+    var curDirection = 0;
+    var prevDirection = 0;
+
+    var header = document.getElementById('header');
+    var toggled;
+    var threshold = 200;
+
+    var checkScroll = function() {
+        curScroll = w.scrollY || doc.scrollTop;
+        if(curScroll > prevScroll) {
+            // scrolled down
+            curDirection = 2;
+        }
+        else {
+            //scrolled up
+            curDirection = 1;
+        }
+
+        if(curDirection !== prevDirection) {
+            toggled = toggleHeader();
+        }
+
+        prevScroll = curScroll;
+        if(toggled) {
+            prevDirection = curDirection;
+        }
+    };
+
+    var toggleHeader = function() { 
+        toggled = true;
+        if(curDirection === 2 && curScroll > threshold) {
+            header.classList.add('hide');
+        }
+        else if (curDirection === 1) {
+            header.classList.remove('hide');
+        }
+        else {
+            toggled = false;
+        }
+        return toggled;
+    };
+
+    window.addEventListener('scroll', checkScroll);
+
+})();
